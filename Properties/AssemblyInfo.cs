31f470af4b664384acf958dd7cdb1066639bbdea
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2020-03-13 08:48:06 -0300

    Fix Assemblyinfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,13 +7,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyIdTravelGateway")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MyIdTravel Upload Profile Staff Gateway")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AFONSOFT")]
 [assembly: AssemblyProduct("MyIdTravelGateway")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("AFONSOFT ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2020.3.14")]
+[assembly: AssemblyFileVersion("2020.3.14")]
+[assembly: NeutralResourcesLanguage("pt-BR")]
